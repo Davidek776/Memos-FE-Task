3f29a253d0f5bd9689f12cf28c7f7f178303da8a
David Kocur <62095094+Davidek776@users.noreply.github.com>
2025-07-12 14:04:18 +0200

    Setup router view.

---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-[url('/path/to/image.svg')] bg-no-repeat bg-center bg-cover h-64 w-64">
+        
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Hello</h1>
+  <HomeHeader />
 </template>
+
+<script setup lang="ts">
+  import HomeHeader from '@/components/HomeHeader.vue';
+</script>
