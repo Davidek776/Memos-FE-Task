832f4980813f095a8eeec07dd268210f1b94ecbc
David Kocur <62095094+Davidek776@users.noreply.github.com>
2025-07-13 20:18:06 +0200

    Format code.

--- src/components/HomeHeader.vue
+++ src/components/HomeHeader.vue
@@ -1,7 +1,5 @@
 <template>
-    <div
-    class="bg-[url('@/assets/Header_Image.svg')] bg-no-repeat bg-center bg-cover aspect-[28/17] bg-[#2e1893d9] bg-blend-lighten flex flex-col items-center"
-    >
+    <div class="bg-[url('@/assets/Header_Image.svg')] bg-no-repeat bg-center bg-cover aspect-[28/17] bg-[#2e1893d9] bg-blend-lighten flex flex-col items-center">
         <div class="flex justify-center pt-21 gap-11">
             <a href="" class="text-lg font-medium" >Home</a>
             <a href="" class="text-lg font-medium" @click.prevent="scrollToSection('featuresSection')">Features</a>
