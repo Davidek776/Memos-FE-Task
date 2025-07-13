0305b4ebfd526f1c8b01200231b0263c770ab6b4
David Kocur <62095094+Davidek776@users.noreply.github.com>
2025-07-13 11:18:22 +0200

    Work on input field.

---
+++ src/components/HomeContact.vue
@@ -0,0 +1,47 @@
+<template>
+    <div
+    class="bg-[url('@/assets/Contact_Image.svg')] bg-no-repeat bg-center bg-cover aspect-[1440/770] bg-[#2e189378] bg-blend-lighten flex items-center justify-center"
+    >
+        <div>
+            <h2 class="text-[42px] font-bold">Let’s Keep in Touch</h2>
+            <p class="text-[22px] font-medium w-100 pt-7">We have created a new product that will help designers, developers and companies create websites for their startups quickly and easily.</p>
+            <a href="tel:15555055050" class="flex text-lg gap-4 pt-13">
+                <font-awesome-icon icon="fa-solid fa-phone text-base text-lg" />
+                <p class="text-base">+1 555 505 5050</p>
+            </a>
+            <a href="mailto:info@designmodo.com" class="flex text-lg gap-4 pt-8">
+                <font-awesome-icon icon="fa-solid fa-envelope text-lg" />
+                <p class="text-base">info@designmodo.com</p>
+            </a>
+            <a href="" class="flex text-lg gap-4 pt-8">
+            <font-awesome-icon icon="fa-regular fa-building text-lg" />
+            <p class="text-base">San Francisco, CA560 Bush St & 
+                <br>20th Ave, Apt5 San Francisco, 
+                <br> 230909
+            </p>
+        </a>
+        </div>
+        <div class="bg-white rounded-[10px] h-120 pt-14.5 px-12.5 flex flex-col items-center">
+            <div class="flex justify-center items-center gap-7.5">
+                <div>
+                    <p class="text-[#1e0e62] text-sm pb-1.5 font-bold tracking-widest">YOUR NAME</p>
+                    <input type="text" placeholder="First name" class="outline-[#ebeaed] outline-2 rounded-4xl w-50 h-12 placeholder:text-[#15143966] text-lg text-[#15143966] pl-6">
+                </div>
+                <div>
+                    <p class="text-[#1e0e62] text-sm pb-1.5 font-bold tracking-widest">BUDGET</p>
+                    <select id="countries" class="outline-[#ebeaed] outline-2 rounded-4xl w-35 h-12 placeholder:text-[#15143966] text-lg text-[#15143966] pl-6 border-r-24 border-transparent ">
+                        <option selected>$500</option>
+                        <option value="$1000">$1000</option>
+                        <option value="$2000">$2000</option>
+                        <option value="$5000">$5000</option>
+                        <option value="$7000">$7000</option>
+                     
+                    </select>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <template>
-  <HomeHeader/>
-  <HomeFeatures/>
-  <HomeAction/>
+  <Home-header/>
+  <Home-features/>
+  <Home-action/>
+  <Home-contact/>
 </template>
 
 <script setup lang="ts">
   import HomeHeader from "@/components/HomeHeader.vue";
   import HomeFeatures from "@/components/HomeFeatures.vue";
   import HomeAction from "@/components/HomeAction.vue";
+  import HomeContact from "@/components/HomeContact.vue";
 </script>
