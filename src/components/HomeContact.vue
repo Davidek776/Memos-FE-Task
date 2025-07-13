ee94975fdb2620401b31c70a59dea892faabe328
David Kocur <62095094+Davidek776@users.noreply.github.com>
2025-07-13 14:52:09 +0200

    Work on styling input checkbox.

--- src/components/HomeContact.vue
+++ src/components/HomeContact.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-    class="bg-[url('@/assets/Contact_Image.svg')] bg-no-repeat bg-center bg-cover aspect-[1440/770] bg-[#2e189378] bg-blend-lighten flex items-center justify-center"
+    class="bg-[url('@/assets/Contact_Image.svg')] bg-no-repeat bg-center bg-cover aspect-[1440/770] bg-[#2e189378] bg-blend-lighten flex items-center justify-center gap-22.5"
     >
         <div>
             <h2 class="text-[42px] font-bold">Let’s Keep in Touch</h2>
@@ -21,7 +21,7 @@
             </p>
         </a>
         </div>
-        <div class="bg-white rounded-[10px] h-120 pt-14.5 px-12.5 flex flex-col items-center gap-8.5">
+        <div class="bg-white rounded-[10px] pt-14.5 px-12.5 flex flex-col items-center ">
             <div class="flex justify-center items-center gap-7.5">
                 <div>
                     <p class="text-[#1e0e62] text-sm pb-1.5 font-bold tracking-widest">YOUR NAME</p>
@@ -39,19 +39,22 @@
                     </select>
                 </div>
             </div>
-              <div class="w-full">
+              <div class="w-full pt-8.5">
                 <p class="text-[#1e0e62] text-sm pb-1.5 font-bold tracking-widest">INPUT FIELD</p>
                 <input type="email" placeholder="name@mail.com" class="outline-[#ebeaed] outline-2 rounded-4xl w-full h-12 placeholder:text-[#15143966]  focus:outline-stone-400 text-lg text-[#15143966] pl-6">
                 </div>
-                <div class="w-full">
+                <div class="w-full pt-8.5">
                     <p class="text-[#1e0e62] text-sm pb-1.5 font-bold tracking-widest">YOUR MESSAGE</p>
                     <textarea placeholder="Message" class="outline-[#ebeaed] outline-2 rounded-[10px] w-full h-28 placeholder:text-[#15143966]  focus:outline-stone-400 text-lg text-[#15143966] px-5 py-3 resize-none"/>
                 </div>
-                <div class="flex">
-                    <label class="flex items-center cursor-pointer">
-                    <input type="checkbox" class="form-checkbox h-5 w-5 checked:bg-amber-300 checked:border-transparent" />
-                    <span class="text-gray-700">Send me a copy</span>
+                <div class="flex justify-between items-center w-full pt-7 pb-14">
+                  <label class="flex items-center space-x-2 cursor-pointer gap-3">
+                    <input type="checkbox" class="form-checkbox h-5 w-5 text-amber-500 accent-[#25dac5] rounded-4xl cursor-pointer"/>
+                    <span class="text-[#15143966] text-base font-normal">Send me a copy</span>
                     </label>
+                 <button class="bg-[#25dac5] rounded-[100px] px-9.5 py-3 hover:bg-teal-300 transition duration-400 ease-in-out cursor-pointer">
+                <p class="text-xl font-medium">Send</p>
+            </button>
                 </div>
         </div>
     </div>
